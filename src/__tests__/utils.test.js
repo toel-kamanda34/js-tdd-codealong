bb1093096956544295ac9aca0332c1084c6ba3cd
toel-kamanda34 <toelokemwa@gmail.com>
2024-08-23 14:33:15 +0300

    completed code along

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,8 +2,16 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+
+    // arrange
     const birthYear = 1984;
+    const currentYear = new Date().getFullYear();
+    const answer = currentYear - birthYear
+
+    //act
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    
+    // assert
+    expect(ageOfPerson).toBe(answer);
   });
 });
